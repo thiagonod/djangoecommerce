37a511452b25bd887ccc9cc40c743b439d2d91c6
ThiagoNod <thiagoints@gmail.com>
2017-11-06 12:18:42 -0200

    Criando core templates

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+
 def index(request):
     text = [
         'Architecto at eligendi ipsum laborum maiores nostrum vel.',
-        'Atque aut dolor fuga harum illum maiores nostrum officia, qui quia rerum vel, voluptatibus.',
+        'Atque aut dolor fuga harum illum maiores nostrum officia, qui quia\
+rerum vel, voluptatibus.',
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     ]
     context = {
-        'title' : 'django e-commerce',
-        'text'  : text,
+        'title': 'django e-commerce',
+        'text': text,
     }
     return render(request, 'index.html', context)
